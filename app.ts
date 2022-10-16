8d6b374c83b14be1211d7c405c113219f5f417fb
David Paucar <soydavidpaucar@gmail.com>
2022-10-16 17:02:41 -0500

    :recycle: updated port and server status

--- app.ts
+++ app.ts
@@ -2,12 +2,12 @@ import express, {Express, Request, Response} from 'express';
 
 const app: Express = express();
 
-const PORT: number = 3000 || process.env.PORT;
+const PORT: number = 8080 || process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
     res.json({message: 'Hello World!'});
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
 })
